3442495e1c38baa5ba04426388ae298501f998a4
Vladimir Borisov <vlad@ugra-tv.ru>
2018-05-05 17:21:31 +0500

    Первое домашнее задание.

--- L1.1/src/main/java/main/Main.java
+++ L1.1/src/main/java/main/Main.java
@@ -3,19 +3,22 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.LoggerFactory;
 import servlets.AllRequestsServlet;
 
 public class Main {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
 
         Server server = new Server(8080);
         server.setHandler(context);
 
         server.start();
+        log.info("Server started");
         server.join();
     }
 }
