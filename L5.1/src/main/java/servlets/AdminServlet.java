7c8432208cb8d99bb535228b5a758162a03e1d2b
Vladimir Borisov <vlad@ugra-tv.ru>
2018-05-17 00:46:55 +0500

    Задание № 5.

---
+++ L5.1/src/main/java/servlets/AdminServlet.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import accountServer.AccountServerI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AdminServlet extends HttpServlet {
+    static final Logger logger = LogManager.getLogger(AdminServlet.class.getName());
+    public static final String PAGE_URL = "/admin";
+    private final AccountServerI accountServer;
+
+    public AdminServlet(AccountServerI accountServer) {
+        this.accountServer = accountServer;
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=utf-8");
+
+        response.getWriter().println(accountServer.getUsersLimit());
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+}
